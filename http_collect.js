0bbbaa3acbe7638108465ccf4560e2f014f45f38
amaralDaniel <amaral94daniel@gmail.com>
2017-01-19 03:42:28 +0000

    http_collect fixed

--- http_collect.js
+++ http_collect.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-//get input
-//perform http response
-//write callback function
-//on "data" output data to user
-//on "error" report error
-//on "end" end
-=======
+
 //require bl
 //get argument
 //perform GET
@@ -13,25 +6,24 @@
 //Note that you will probably need to data.toString() to convert from a Buffer.
 //on("end") print the info
 
->>>>>>> 651d4d039f3dc9e09e2520a73bdfe8d1b4e3eb54
+
 var bl = require('bl');
 var http = require('http');
 
 function output_function(response){
   response.setEncoding('utf8');
   response.pipe(bl(function (err, data) {
-<<<<<<< HEAD
+
     if(err){
       return console.log(err);
     }
-=======
->>>>>>> 651d4d039f3dc9e09e2520a73bdfe8d1b4e3eb54
+
     console.log(data.length);
     console.log(data.toString()); }));
 }
 
 http.get(process.argv[2],output_function);
-<<<<<<< HEAD
+
 
     // Provided solution
     //
@@ -49,5 +41,3 @@ http.get(process.argv[2],output_function);
     //     console.log(data)
     //   }))
     // })
-=======
->>>>>>> 651d4d039f3dc9e09e2520a73bdfe8d1b4e3eb54
