53fef4b9530362d6ef9f7e7b3686c7f0b178b67c
amaralDaniel <amaral94daniel@gmail.com>
2017-01-21 01:23:41 +0000

    started file server

---
+++ http_file_server.js
@@ -0,0 +1,13 @@
+//require http
+//http.createServer() => create http server
+//put server listening
+
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function (request, response){
+  var fileStream = fs.createReadStream(process.argv[3]);
+  request.pipe(fileStream);
+});
+
+server.listen(process.argv[2]);
